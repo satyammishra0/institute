ab2e580c82182a28843fe36f1095147e3d23ab55
satyam mishra <satyam.growupnext@gmail.com>
2022-12-03 13:36:21 +0530

    new db defiened

--- configuration.php
+++ configuration.php
@@ -2,10 +2,16 @@
 //  --------------------------- 
 //  Connection to the database 
 //  --------------------------- 
+// $serverName = 'localhost';
+// $dbuserName = 'root';
+// $password = '';
+// $dbName = "institute";
+
 $serverName = 'localhost';
-$dbuserName = 'root';
-$password = '';
+$dbuserName = 'u809849815_user';
+$password = 'Satyammi_007';
 $dbName = "institute";
+
 
 
 $conn = mysqli_connect($serverName, $dbuserName, $password, $dbName);
